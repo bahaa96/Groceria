7987ae692605d4ec406848325852f5e742f02a47
Bahaa jr <a.bahaa@dailymealz.app>
2020-05-14 19:27:47 +0200

    initial commit

---
+++ src/config/api.js
@@ -0,0 +1,10 @@
+export const API = 'https://groceria.co.uk';
+
+export const CONSUMER_KEY = 'ck_34eccca54ece0737f80563668d243df0811d42ca';
+export const CONSUMER_SECRET = 'cs_26dc233d6e4788d4ceb76b4ed08cb83340de0358';
+
+export default {
+  API_ENDPOINT: API,
+  CONSUMER_KEY,
+  CONSUMER_SECRET
+};
